d1ca29c08a657659e99e763c6697c266106c100e
Michael <kobelu1990@gmail.com>
2015-11-21 20:25:19 +0800

    complete function

---
+++ download_img.rb
@@ -0,0 +1,29 @@
+require 'open-uri'
+require 'nokogiri'
+
+module Tieba
+	module Post
+		def self.fetch_img_url(url, pagenum)
+			@img_url = []
+			(1..pagenum).each do |num|
+				doc = Nokogiri::HTML(open(url+"?pn=#{num}"))
+				doc.css('.d_post_content>img.BDE_Image').each do|content|
+					@img_url.push content.attribute('src').to_s.sub(/w\%3D580\/sign.*\//,"pic\/item\/")
+				end
+			end
+		end
+
+		def self.download_img(url, pagenum)
+			fetch_img_url(url, pagenum)
+			@img_url.each_with_index do |url,index|
+				open(url) {|f|
+				   File.open("#{index}.jpg","wb") do |file|
+				     file.puts f.read
+				   end
+				}
+			end
+		end
+	end
+end
+
+Tieba::Post.download_img(ARGV[0],ARGV[1].to_i)
